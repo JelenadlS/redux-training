0b75d74872e1b649bc5f6913abcd41021b57e732
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-04-15 13:45:04 +0200

    Add rating

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,13 +8,39 @@ import { restaurantLiked } from "./restaurantsSlice";
 
 export default function RestaurantCard({ data }) {
   const [show, setShow] = useState(false);
+
   const dispatch = useDispatch();
 
   const bookmarks = useSelector((state) => state.bookmarksReducer.data);
 
+  const filledStar = <StyledStar color icon="bi:star-fill" />;
+  const halfStar = <StyledStarHalf color icon="bi:star-half" />;
+  const emptyStar = <Icon color icon="bi:star" />;
+  const calculatedRating =
+    (5 / 10) * (data.phone === null ? "0" : data.phone.slice(0, 1));
+
+  const rating = [
+    (calculatedRating === 0 && emptyStar) ||
+      (calculatedRating === 0.5 && halfStar) ||
+      (calculatedRating >= 1 ? filledStar : emptyStar),
+    (calculatedRating === 1.5 && halfStar) ||
+      (calculatedRating >= 2 ? filledStar : emptyStar),
+    (calculatedRating === 2.5 && halfStar) ||
+      (calculatedRating >= 3 ? filledStar : emptyStar),
+    (calculatedRating === 3.5 && halfStar) ||
+      (calculatedRating >= 4 ? filledStar : emptyStar),
+    (calculatedRating === 4.5 && halfStar) ||
+      (calculatedRating >= 5 ? filledStar : emptyStar),
+  ];
+
   return (
     <StyledCard>
-      <p>rating</p>
+      <StyledRating>
+        {(5 / 10) * (data.phone === null ? "0" : data.phone.slice(0, 1))}
+        {rating.map((rate, index) => (
+          <li key={index}>{rate}</li>
+        ))}
+      </StyledRating>
       <StyledName>{data.name}</StyledName>
       <StyledLike
         with="35px"
@@ -84,4 +110,17 @@ const StyledAvailability = styled.p`
 const StyledLocation = styled(Icon)`
   grid-column: 4;
   justify-self: end;
+`;
+
+const StyledStar = styled(Icon)`
+  color: ${(props) => (props.color ? "yellow" : "black")};
+`;
+
+const StyledStarHalf = styled(Icon)`
+  color: ${(props) => (props.color ? "yellow" : "black")};
+`;
+
+const StyledRating = styled.p`
+  display: flex;
+  gap: 5px;
 `;
