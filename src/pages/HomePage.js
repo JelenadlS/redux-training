3b0465d4f7baced2e86f2d7d514c33ececd72e80
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-04-19 11:18:10 +0200

    Fetch data

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,17 @@
 import { StyledMain } from "../components/styles";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchRestaurants } from "../redux/Slices";
 
 export default function HomePage() {
+  const dispatch = useDispatch();
+  const restaurants = useSelector((state) => state);
+
+  console.log(restaurants);
+
+  useEffect(() => {
+    dispatch(fetchRestaurants());
+  }, [dispatch]);
+
   return <StyledMain>Home page</StyledMain>;
 }
--- src/redux/Slices.js
+++ src/redux/Slices.js
@@ -1,9 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchRestaurants = createAsyncThunk(
+  "restaurants/fetchRestaurants",
+  async () => {
+    try {
+      return fetch(`https://api.openbrewerydb.org/breweries`).then((response) =>
+        response.json()
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 const restaurantSlice = createSlice({
   name: "restaurant",
-  initialState: {},
-  reducer: {},
+  initialState: {
+    list: [],
+    status: null,
+  },
+  extraReducers: {
+    [fetchRestaurants.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [fetchRestaurants.fulfilled]: (state, action) => {
+      state.list = action.payload;
+      state.status = "success";
+    },
+    [fetchRestaurants.rejected]: (state, action) => {
+      state.status = "failed";
+    },
+  },
 });
 
 export default restaurantSlice.reducer;
