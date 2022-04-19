b2df809e4bf52c1c3b941b6ee84ea1bd1c87f5e6
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-04-19 17:59:05 +0200

    Add picture array

---
+++ src/components/Pictures.js
@@ -0,0 +1,15 @@
+import images from "../images";
+
+const Pictures = [
+  { name: "ausblick", image: images.Ausblick },
+  { name: "freunde", image: images.Freunde },
+  { name: "see", image: images.See },
+  { name: "strandAbend", image: images.StrandAbend },
+  { name: "stranddecke", image: images.Stranddecke },
+  { name: "strandLesen", image: images.StrandLesen },
+  { name: "strandliegen", image: images.Strandliegen },
+  { name: "wandern", image: images.Wandern },
+  { name: "wohnwagen", image: images.Wohnwagen },
+];
+
+export default Pictures;
