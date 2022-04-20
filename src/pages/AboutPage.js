dd2bc3f821a78b77e810be732df40bcaf5dfa81d
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-04-20 11:24:47 +0200

    Add details to about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,3 +1,18 @@
+import { StyledMain } from "../components/styles";
+import { AboutText } from "../components/AboutText";
+
 export default function AboutPage() {
-  return <div>About</div>;
+  return (
+    <StyledMain aria-label="about page">
+      <h1>about us</h1>
+      {AboutText.map((detail, index) => {
+        return (
+          <article key={index} aria-label={detail.title}>
+            <h2>{detail.title}</h2>
+            <p aria-label="further information">{detail.details}</p>
+          </article>
+        );
+      })}
+    </StyledMain>
+  );
 }
