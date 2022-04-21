217f6cae78ecc28f94a4a2d6ab8bfbdbd09d2031
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-04-21 20:34:29 +0200

    display about text

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,3 +1,18 @@
+import { StyledMain } from "../components/styles";
+import { AboutText } from "../data/AboutText";
+
 export default function AboutPage() {
-  return <div>About</div>;
+  return (
+    <StyledMain>
+      <h1>About</h1>
+      {AboutText.map((text, index) => {
+        return (
+          <section key={index}>
+            <h2>{text.title}</h2>
+            <p aria-label="further info">{text.details}</p>
+          </section>
+        );
+      })}
+    </StyledMain>
+  );
 }
