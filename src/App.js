b3714172bf2ec04fba16490352bec61fd0936c6d
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-04-21 17:05:41 +0200

    Add navigation

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,5 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navigation() {
+  return <NavLink> </NavLink>;
+}
