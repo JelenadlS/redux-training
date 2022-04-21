cf5dfe9730ac3d9e0377683e173c31db59ee8bf8
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-04-21 17:38:06 +0200

    Fetch data

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { StyledMain } from "../components/styles";
 
+import { fetchRestaurants } from "../redux/Slices";
+
 export default function HomePage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRestaurants());
+  }, [dispatch]);
+
   return (
     <StyledMain>
       <h1>Home</h1>
--- src/redux/Slices.js
+++ src/redux/Slices.js
@@ -1,5 +1,19 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+export const fetchRestaurants = createAsyncThunk(
+  "restaurants/fetchRestaurants",
+  async () => {
+    try {
+      const response = await fetch(
+        `https://api.openbrewerydb.org/breweries`
+      ).then((data) => data.json());
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 const restaurantsSlice = createSlice({
   name: "restaurants",
   initialState: { restaurantList: [] },
