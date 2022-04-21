689f473d481090642e3223ee8131490f969f5331
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-04-21 17:00:47 +0200

    Add homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 }
 
 export default App;
